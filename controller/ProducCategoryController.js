3d3530345260e2a4d60e623a3b30b8db6d487b1c
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-17 20:32:38 +0545

    create findall api in productcategory controller

---
+++ controller/ProducCategoryController.js
@@ -0,0 +1,16 @@
+const ProductCategory = require("../model/ProductCategory");
+const { param } = require("../route/productCategoryRoute");
+const findAll = async (req, res) => {
+    try {
+        const category = await ProductCategory.find();
+        res.status(200).json(category);
+    } catch (e) {
+        res.json(e)
+    }
+}
+
+
+
+module.exports = {
+    findAll,
+}
