b5c504b82ecfe153719a845a46ff7fe48924d999
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-17 20:41:53 +0545

    import productcategory route in app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const connectDb = require("./config/db")
 const CustomerRouter = require("./route/customerRoute")
+const CategoryRouter = require("./route/productCategoryRoute")
+
 const app = express();
 
 connectDb();
 
 app.use(express.json());
 app.use("/api/customer", CustomerRouter);
+app.use("/api/category", CategoryRouter);
 
 const port = 3000;
 app.listen(port, () => {
