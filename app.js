a5e23bd4b58406831562afd0a7f86eac2431c467
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-17 20:23:59 +0545

    create productcategory model

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app = express();
 connectDb();
 
 app.use(express.json());
-app.use("/api/customer",CustomerRouter );
+app.use("/api/customer", CustomerRouter);
+
 const port = 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
 })
