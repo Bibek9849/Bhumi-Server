adedad02cdf96a579e01c90a531e453d5b062371
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-17 21:48:25 +0545

    modify customer controller and customer route for storing user images

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -11,7 +11,14 @@ const findAll = async (req, res) => {
 
 const save = async (req, res) => {
     try {
-        const customer = new Customer(req.body);
+        const { fullname, contact_no, address, password } = req.body
+        const customer = new Customer({
+            fullname,
+            contact_no,
+            address,
+            password,
+            image: req.file.originalname
+        });
         await customer.save();
         res.status(201).json(customer)
     } catch (e) {
@@ -19,30 +26,30 @@ const save = async (req, res) => {
     }
 }
 
-const findbyId = async(req,res)=>{
-    try{
+const findbyId = async (req, res) => {
+    try {
         const customer = await Customer.findById(req.params.id);
         res.status(200).json(customer)
-    }catch (e){
+    } catch (e) {
 
         res.json(e)
     }
 }
-const deletebyId = async(req,res)=>{
-    try{
+const deletebyId = async (req, res) => {
+    try {
         const customer = await Customer.findByIdAndDelete(req.params.id);
         res.status(200).json("Data Deleted")
-    }catch (e){
+    } catch (e) {
 
         res.json(e)
     }
 }
 
-const update = async(req,res)=>{
-    try{
-        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {new:true});
+const update = async (req, res) => {
+    try {
+        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(201).json(customer)
-    }catch (e){
+    } catch (e) {
 
         res.json(e)
     }
