391f3231ffd622939ab32bb577c77a437a38aaec
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-23 20:58:32 +0545

    create user model

---
+++ model/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    contact_no: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model("users", userSchema)
+module.exports = User;
