a92fc100928515194afa5c24d6bff4de303c6341
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-23 21:27:26 +0545

    adding path of folder in user route to store images of users

--- route/userRoute.js
+++ route/userRoute.js
@@ -2,9 +2,20 @@ const express = require("express");
 const { findAll, save, findbyId, deletebyId, update } = require("../controller/userController");
 const router = express.Router();
 
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: function (req, res, cb) {
+        cb(null, 'user_images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage })
+
 
 router.get("/", findAll);
-router.post("/", save);
+router.post("/", upload.single('file'), save);
 router.get("/:id", findbyId)
 router.delete("/:id", deletebyId)
 router.put("/:id", update)
