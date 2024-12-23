d2388331029f050924c16c431b46b5fbbe43e107
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-23 21:29:18 +0545

    adding path of userRoute in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const connectDb = require("./config/db")
+const UserRouter = require("./route/userRoute")
+
 
 
 
@@ -8,6 +10,7 @@ const app = express();
 connectDb();
 
 app.use(express.json());
+app.use("/api/user", UserRouter);
 
 
 const port = 3000;
