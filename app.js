a775eb347c50a65f85e626e3a62d1476297d9e44
Bibek Pandey <pandeybibe098k@gmail.com>
2025-01-03 15:48:30 +0545

    creating order route and import in app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const connectDb = require("./config/db")
 const UserRouter = require("./route/userRoute")
 const CategoryRouter = require("./route/productCategoryRoute")
 const TypeRouter = require("./route/productTypeRoute")
-const Product = require("./route/productRoute")
+const ProductRouter = require("./route/productRoute")
+const OrderRouter = require("./route/orderRoute")
 
 
 const app = express();
@@ -14,8 +15,8 @@ app.use(express.json());
 app.use("/api/user", UserRouter);
 app.use("/api/category", CategoryRouter);
 app.use("/api/type", TypeRouter);
-
-app.use("/api/product", Product);
+app.use("/api/product", ProductRouter);
+app.use("/api/order", OrderRouter);
 
 const port = 3000;
 app.listen(port, () => {
