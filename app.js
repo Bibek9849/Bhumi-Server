d134049969149285ce356a288b54fffae7d39604
Bibek Pandey <pandeybibe098k@gmail.com>
2025-01-06 20:17:52 +0545

    created payment model

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const CategoryRouter = require("./route/productCategoryRoute")
 const TypeRouter = require("./route/productTypeRoute")
 const ProductRouter = require("./route/productRoute")
 const OrderRouter = require("./route/orderRoute")
+const OrderDetailRouter = require("./route/orderDetailRoute")
+const FeedbackRouter = require("./route/feedbackRoute")
 
 
 const app = express();
@@ -17,6 +19,8 @@ app.use("/api/category", CategoryRouter);
 app.use("/api/type", TypeRouter);
 app.use("/api/product", ProductRouter);
 app.use("/api/order", OrderRouter);
+app.use("/api/details", OrderDetailRouter);
+app.use("/api/feedback", FeedbackRouter);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ model/payment.js
@@ -0,0 +1,28 @@
+const { date } = require("joi");
+const mongoose = require("mongoose")
+const paymentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    },
+    productID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product"
+    },
+
+    payment_method: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    }
+})
+
+const payment = mongoose.model("payments", paymentSchema)
+module.exports = payment;
