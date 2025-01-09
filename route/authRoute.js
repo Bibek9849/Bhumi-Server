a6c042ba07954f86cd9215936adaf8bd1b103d70
Bibek Pandey <pandeybibe098k@gmail.com>
2025-01-09 18:31:59 +0545

    created auth route

---
+++ route/authRoute.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router();
+const { login, register } = require("../controller/authController");
+const { authenticateToken } = require("../security/auth");
+
+
+router.post("/login", login);
+router.post("/register", authenticateToken, register);
+
+
+
+module.exports = router;
